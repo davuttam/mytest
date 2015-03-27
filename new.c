941f1a93aba27e999b7689646baa3a5d13b5df42
davuttam <davuttam@gmail.com>
2015-03-27 21:28:01 +0530

    my_first_commit

---
+++ new.c
@@ -0,0 +1,11 @@
+//this is "C" program of two number additions;;
+
+#include<stdio.h>
+int main()
+{
+int x,y,c;
+printf("enter the value of x and y")
+scanf("%d %d",&x,&y);
+c=a+b;
+printf("addition of two number is %d",c);
+}
